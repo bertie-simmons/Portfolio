efa513c1ca19a87dea5da765e5dc16b6479c35f2
Bertie <bertiesimmons06@gmail.com>
2025-11-24 21:39:40 +0000

    updated readme.md and also made the site compatible to be deployed on github

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "astro/config";
 import tailwind from "@tailwindcss/postcss";
 
 export default defineConfig({
+  site: 'https://bertie-simmons.github.io',
+  base: '/Portfolio',
   vite: {
     css: {
       postcss: {
